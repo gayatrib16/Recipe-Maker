d5ae7182819db591b14a03369e46b8f7b72c6880
Gayatri <gayatrisbadgujar1606@gmail.com>
2024-01-02 15:50:29 +0530

    added jdsfksdfjksdf

--- FINAL/api/src/index.js
+++ FINAL/api/src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-// const { userRouter } = require("./routes/user.js");
-// const { recipesRouter } = require("./routes/recipes.js"); // Corrected import
+const { userRouter } = require("./routes/user.js");
+const { recipesRouter } = require("./routes/recipes.js"); // Corrected import
 const app = express();
 const dotenv=require("dotenv")
 dotenv.config({path:"config.env"})
@@ -11,23 +11,23 @@ app.use(express.json());
 app.use(cors());
 
 
-// const dbConnection = async() => {
-//   await mongoose
-//     .connect('mongodb+srv://gayatrisbadgujar1606:Study$16@recipes.43oeohq.mongodb.net', {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     .then(() => {
-//       console.log("Connected to the database");
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// };
+const dbConnection = async() => {
+  await mongoose
+    .connect('mongodb+srv://gayatrisbadgujar1606:Study$16@recipes.43oeohq.mongodb.net', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to the database");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
 
-// dbConnection();
+dbConnection();
 app.use("/", startRoutes);
-// app.use("/auth", userRouter);
-// app.use("/recipes", recipesRouter); // Corrected usage
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter); // Corrected usage
 app.listen(PORT, () => console.log("Server started on port 3001"));
 
