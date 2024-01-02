1b9f48a02c204bcf2447489d45695374855d90b5
Gayatri <gayatrisbadgujar1606@gmail.com>
2024-01-02 15:32:24 +0530

    added jdsfksdfjksdf

--- FINAL/api/src/index.js
+++ FINAL/api/src/index.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const { userRouter } = require("./routes/user.js");
-const { recipesRouter } = require("./routes/recipes.js"); // Corrected import
+// const { userRouter } = require("./routes/user.js");
+// const { recipesRouter } = require("./routes/recipes.js"); // Corrected import
 const app = express();
 const dotenv=require("dotenv")
 dotenv.config({path:"config.env"})
 const PORT=process.env.PORT
 app.use(express.json());
 app.use(cors());
-app.use("/", startRoutes);
-app.use("/auth", userRouter);
-app.use("/recipes", recipesRouter); // Corrected usage
 
-const dbConnection = () => {
-  mongoose
+
+const dbConnection = async() => {
+  await mongoose
     .connect('mongodb+srv://gayatrisbadgujar1606:Study$16@recipes.43oeohq.mongodb.net', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -28,5 +26,8 @@ const dbConnection = () => {
 };
 
 dbConnection();
+app.use("/", startRoutes);
+// app.use("/auth", userRouter);
+// app.use("/recipes", recipesRouter); // Corrected usage
 app.listen(PORT, () => console.log("Server started on port 3001"));
 
